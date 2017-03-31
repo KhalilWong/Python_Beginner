7ed48b923c84932e3d81c74369b808cf9f9e47ec
Khalil Wong <1039906090@qq.com>
2017-03-31 14:10:06 +0800

    Update split_data_timestep.py

--- split_data_timestep.py
+++ split_data_timestep.py
@@ -1,19 +1,18 @@
-line=''
-try:
-    with open('bigdata.test') as bdata:
-        for each_line in bdata:
-            line+=each_line
-        sdata=line.split('ITEM: TIMESTEP')
-        del sdata[0]
-        l=len(sdata)
-except IOError as err:
-    print('File Error:'+str(err))
-
-for i in range(l):
+def split_data_timestep(file):
+    line=''
     try:
-        with open(str(i)+'.test','w') as out:
-            print(sdata[i],file=out)
-        get_data(str(i)+'.test')
-        shumidu(TimeStep,NOAtoms,XX,YY,ZZ)
+        with open(file) as bdata:
+            for each_line in bdata:
+                line+=each_line                                                #所有行数据串成一个字符串
+            sdata=line.split('ITEM: TIMESTEP')                                 #按时间步分割字符串
+            del sdata[0]
+            l=len(sdata)
     except IOError as err:
         print('File Error:'+str(err))
+
+    for i in range(l):
+        try:
+            with open(str(i)+'.split','w') as out:
+                print(sdata[i],file=out)                                       #按时间步生成多个数据文件
+        except IOError as err:
+            print('File Error:'+str(err))
